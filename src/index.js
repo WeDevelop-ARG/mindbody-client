5e0881d9f008ce64ce2be393f80ff672faf75cce
Roque Alejandro Cuello <rcool77@gmail.com>
2020-06-22 15:41:14 -0300

    Implement Site Mixin including activation methods

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import { flow } from 'lodash'
 import BaseClient from './classes/BaseClient'
 import AuthenticationMixin from './mixins/AuthenticationMixin'
 import ClientsMixin from './mixins/ClientsMixin'
+import SiteMixin from './mixins/SiteMixin'
 
 const applyMixins = flow(
   AuthenticationMixin,
-  ClientsMixin
+  ClientsMixin,
+  SiteMixin
 )
 const MindbodyClient = applyMixins(BaseClient)
 
